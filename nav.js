e64672a65472bac1362ec7ac0e0dd79d687b25ae
div2005 <div2005@tuta.io>
2021-07-07 16:05:44 +0300

    Update

---
+++ nav.js
@@ -0,0 +1,12 @@
+document.write('\
+\
+<nav class="navigation">\
+<ul>\
+<li><a href="/"><i class="fas fa-home"></i> Home</a></li>\
+<li><a href="#"><i class="fas fa-book"></i> Blog</a></li>\
+<li><a href="v1/index.html"><i class="fas fa-folder"></i> v1</a></li>\
+<li class="user"><a><img src="assets/gg.jpg"> div2005</a></li>\
+</ul>\
+</nav>\
+\
+');
