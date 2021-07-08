d3ec305c1ced5f035ad822730027fe834c38f33b
div2005 <div2005@tuta.io>
2021-07-08 17:52:58 +0300

    Blog testing

--- nav.js
+++ nav.js
@@ -5,7 +5,7 @@ document.write('\
 <li><a href="/"><i class="fas fa-home"></i> Home</a></li>\
 <li><a href="/blog"><i class="fas fa-book"></i> Blog</a></li>\
 <li><a href="/v1/"><i class="fas fa-folder"></i> Site v1</a></li>\
-<li class="user"><a><img src="assets/gg.jpg"> div2005</a></li>\
+<li class="user"><a><img src="https://pbs.twimg.com/profile_images/936667323850743808/z_wlQVnF.jpg"> div2005</a></li>\
 </ul>\
 </nav>\
 \
