e287304bb342e43d7e7759ac6513ff33a32ed8cc
div2005 <div2005@tuta.io>
2021-07-10 15:39:07 +0300

    Update

---
+++ copy.js
@@ -0,0 +1,12 @@
+function copyEmail(){
+    navigator.clipboard.writeText("div2005.kk@gmail.com");
+}
+function copyXMPP(){
+    navigator.clipboard.writeText("div2005@darkn.space");
+}
+function copyDiscord(){
+    navigator.clipboard.writeText("div2005#9630");
+}
+function copyRevolt(){
+    navigator.clipboard.writeText("div");
+}
--- nav.js
+++ nav.js
@@ -3,7 +3,7 @@ document.write('\
 <nav class="navigation">\
 <ul class="topmenu">\
 <li><a href="/"><i class="fas fa-home"></i> Home</a></li>\
-<li><a href="/blog"><i class="fas fa-book"></i> Blog</a></li>\
+<li><a href="#"><i class="fas fa-book"></i> Blog</a></li>\
 <li><a href="/v1/"><i class="fas fa-folder"></i> Site v1</a></li>\
 <li class="user"><a><img src="https://pbs.twimg.com/profile_images/936667323850743808/z_wlQVnF.jpg"> div2005</a></li>\
 </ul>\
