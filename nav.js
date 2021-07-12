4fb4adb22bea5bd77a19d16cb3383dfcd3d80dc3
div2005 <div2005@tuta.io>
2021-07-12 22:46:10 +0300

    Update

--- nav.js
+++ nav.js
@@ -5,11 +5,11 @@ document.write('\
             <a href="/"><i class="fas fa-home"></i> Home</a>\
             <a href="/v1/"><i class="fas fa-folder"></i> Site v1</a>\
         </div>\
-        <div class="nav-user">\
-            <img src="assets/gg.jpg">\
-            <span>div2005</span>\
-        </div>\
         </nav>\
 \
 ');
-/* <li><a href="#"><i class="fas fa-book"></i> Blog</a></li>\ */
+/* <li><a href="#"><i class="fas fa-book"></i> Blog</a></li>\ 
+        <div class="nav-user">\
+            <img src="assets/gg.jpg">\
+            <span>div2005</span>\
+        </div>\*/
